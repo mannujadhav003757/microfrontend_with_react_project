4820405ba6b8f1c5403a8fdc9dae11d689676f1b
Manoj Jadhav <manoj.jadhav@neosoftmail.com>
2023-04-21 10:39:10 +0530

    container changes1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import { mount } from "marketing/MarketingApp"
 import MarketingApp from "./components/MarketingApp";
 export default function App(){
     return <>
-    <h1>Hello here is  conta inerhelooo here is some change</h1>
+    <h1>Hello here is  containerhelooo here is some change</h1>
     <MarketingApp />
     </>
 }
