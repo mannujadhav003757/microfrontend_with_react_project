f3886254116409b4b87ab13efd74025191d9a5b6
Manoj Jadhav <manoj.jadhav@neosoftmail.com>
2023-04-21 12:55:05 +0530

    t8

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import { mount } from "marketing/MarketingApp"
 import MarketingApp from "./components/MarketingApp";
 export default function App(){
     return <>
-    <h1>Hello here is  container helo reis   ome change</h1>
+    <h1>Hello here is  container helo reis  ome change</h1>
     <MarketingApp />
     </>
 }
