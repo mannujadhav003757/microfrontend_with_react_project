f0e42a92167c9956af1032e14f2bb9fb22793c84
Manoj Jadhav <manoj.jadhav@neosoftmail.com>
2023-04-24 12:57:12 +0530

    fixing css classname collisions de and prod

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { mount } from "marketing/MarketingApp"
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider,createGenerateClassName } from "@material-ui/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+
+// Here we create by default generate classname's using "ma" prefix
+// for avoiding micro-frontend application css ambigiuity or duplication on production
+
+
+const generateClassName = createGenerateClassName({
+    productionPrefix : "co"
+})
 export default function App(){
     return <>
+    
     <BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
     <div>
     <Header/>
     <MarketingApp />
     </div>
+    </StylesProvider>
     </BrowserRouter>
     </>
 }
